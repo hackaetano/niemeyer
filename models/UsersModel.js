5711a19ebdd1dee76de592a271d9ca3d13181b38
Wil Fernandes <wilmar.wfsj@gmail.com>
2017-11-15 04:20:15 -0200

    Fix: users model

--- models/UsersModel.js
+++ models/UsersModel.js
@@ -1,5 +1,27 @@
 'use strict';
 
+function isObject(item) {
+    return (item && typeof item === 'object' && !Array.isArray(item));
+}
+
+function mergeDeep(target, source) {
+    let output = Object.assign({}, target);
+    if (isObject(target) && isObject(source)) {
+        Object.keys(source).forEach(key => {
+            if (isObject(source[key])) {
+                if (!(key in target))
+                    Object.assign(output, { [key]: source[key] });
+                else
+                    output[key] = mergeDeep(target[key], source[key]);
+            } else {
+                Object.assign(output, { [key]: source[key] });
+            }
+        });
+    }
+    return output;
+}
+
+
 class UsersModel {
     constructor(model) {
         this.model = model;
@@ -32,8 +54,16 @@ class UsersModel {
             _id: _id
         };
 
-        this.model.update(query, data).exec(function(err, result, rows) {
-            callback(err, result, rows);
+        this.model.findOne(query).exec(user => {
+            if (user) {
+                var user = mergeDeep(user, data);
+            } else {
+                var user = data;
+            }
+
+            this.model.update(query, user).exec(function(err, result, rows) {
+                callback(err, result, rows);
+            });
         });
     }
 
@@ -101,7 +131,7 @@ module.exports = function(mongoose) {
             },
             property: {
                 settings: {
-                    type: String,
+                    propertyType: String,
                     size: Number,
                     pricing: Number,
                     rooms: Number
