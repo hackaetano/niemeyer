4bdf7fd815ccc09e17bf732c0ecf36b1e06c7938
Wil Fernandes <wilmar.wfsj@gmail.com>
2017-11-15 11:20:00 -0200

    Add: Matches Controller endpoint

---
+++ controllers/MatchesController.js
@@ -0,0 +1,107 @@
+const Promise = require('bluebird');
+
+function _handleNotFound(data) {
+    if (!data) {
+        let err = new Error('Not found');
+        err.status = 404;
+
+        throw err;
+    }
+
+    return data;
+}
+
+function _mediumPricePerArea(data) {
+    let sum = 0;
+
+    if (!data.length) {
+        return 0;
+    }
+
+    for(var prop in data.results) {
+        let item = data.results[prop];
+        if (item.characteristics) {
+            let pricePerArea = item.characteristics.pricePerArea || 0;
+            if (pricePerArea) {
+                sum = sum + pricePerArea;
+            }
+        }
+    }
+
+    return sum / data.length;
+}
+
+class MatchesController {
+    constructor(user, property) {
+        this.user = Promise.promisifyAll(user);
+        this.property = Promise.promisifyAll(property);
+    }
+
+    getMatches(req, res, next) {
+        let _id = req.params._id;
+
+        this.user.findOneAsync(_id)
+        .then(_handleNotFound)
+        .then(this.parseUser)
+        .then(this.buildQuery)
+        .then(query => {
+            this.property.findAsync(query)
+            .then(data => {
+                let response = {};
+                response.length = data.length;
+                response.results = data;
+                response.mediumPricePerArea = _mediumPricePerArea(response);
+                
+                res.json(response);
+            })
+            .catch(next);
+        })
+        .catch(next);
+    }
+
+    parseUser(data) {
+        data = data.toObject();
+
+        let userToMatch = {};
+        userToMatch.location = data.preferences.neighborhood.settings.district[0];
+        userToMatch.children = data.familiar.children.has;
+        userToMatch.children = data.familiar.children.has;
+        userToMatch.income = data.personal.income;
+
+        return userToMatch;
+    }
+
+    buildQuery(data) {
+        let query = {};
+        query.features = {};
+        query.features.essencials = {};
+        query.characteristics = {};
+
+        query.location = {};
+
+        if (data.children > 0) {
+            query.features.essencials.rooms = {
+                $gt: 1
+            }
+        }
+
+        if (data.district) {
+            query.location.district = new RegExp(data.district, 'gi');
+        }
+
+        query.characteristics.categories = new RegExp('ALUGUEL', 'gi');
+
+        if (data.income) {
+            query.characteristics.princing = {};
+            query.characteristics.princing.rent = {
+                $lt: data.income * 0.4
+            }
+        }
+
+        return query;
+    }
+}
+
+module.exports = function(user, property) {
+    return new MatchesController(user, property);
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('./auth');
-const users = require('./users');
-const properties = require('./properties');
-const brain = require('./brain');
+
+const mongoose = require('../db/mongoose');
+const UserModel = require('../models/UsersModel')(mongoose);
+const PropertyModel = require('../models/PropertiesModel')(mongoose);
 
 router.get('/', (req, res) => {
     let info = {};
@@ -18,9 +19,30 @@ router.get('/', (req, res) => {
 });
 
 router.use('/auth', auth);
-router.use('/users', users);
-router.use('/properties', properties);
 
-router.use('/brain', brain);
+
+// Handle Users
+const Users = require('../controllers/UsersController')(UserModel);
+router.get('/users', Users.getAll.bind(Users));
+router.post('/users', Users.create.bind(Users));
+router.get('/users/:_id', Users.getById.bind(Users));
+router.put('/users/:_id', Users.update.bind(Users));
+router.delete('/users/:_id', Users.delete.bind(Users));
+
+
+// Handle Properties
+const Properties = require('../controllers/PropertiesController')(PropertyModel);
+router.get('/properties', Properties.getAll.bind(Properties));
+router.post('/properties', Properties.create.bind(Properties));
+router.get('/properties/:_id', Properties.getById.bind(Properties));
+router.put('/properties/:_id', Properties.update.bind(Properties));
+router.delete('/properties/:_id', Properties.delete.bind(Properties));
+
+
+// Handle Matches
+const Matches = require('../controllers/MatchesController')(UserModel, PropertyModel);
+router.get('/users/:_id/matches', Matches.getMatches.bind(Matches));
+
+
 
 module.exports = router;
